e5abe246e7a0fca982c7ed5d2164714ccf75ce03
chetantarale <chetan@shippable.com>
2016-03-21 17:49:27 +0530

    Update sql.php

--- sql.php
+++ sql.php
@@ -8,8 +8,9 @@ class SQL {
     mysqli_query($con, $q);
     $q="INSERT INTO things(name) VALUES('Dre')";
     mysqli_query($con, $q);
-    $q="SELECT * FROM users";
-    return $q;
+    $result = mysql_query("SELECT * FROM users");
+    #$q="SELECT * FROM users";
+    return $$result;
   }
 }
 ?>
